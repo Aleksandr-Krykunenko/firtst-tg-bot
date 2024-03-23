97c0f7b772819f5f7733a16bf240c715b992bad8
Aleksandr <aleksandr.krykunenko2206@gmail.com>
2024-03-23 23:49:54 +0200

    chore(project): Configure simple chat example.

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,13 @@
-console.info("hello world")
+import { Telegraf } from "telegraf"
+import { message } from "telegraf/filters"
+
+const bot = new Telegraf("6752689709:AAH1M618npo0-i-0frM-LDyXrvFq9asNngU")
+bot.start((ctx) => ctx.reply('Welcome'))
+bot.help((ctx) => ctx.reply('Send me a sticker'))
+bot.on(message('sticker'), (ctx) => ctx.reply('👍'))
+bot.hears('hi', (ctx) => ctx.reply('Hey pidor'))
+bot.launch()
+
+// Enable graceful stop
+process.once('SIGINT', () => bot.stop('SIGINT'))
+process.once('SIGTERM', () => bot.stop('SIGTERM'))
